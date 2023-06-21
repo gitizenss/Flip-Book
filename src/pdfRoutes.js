e2a49e9420924d0564ab3278901513847db03739
Maher Lawand <maher.lawand9@gmail.com>
2023-06-21 18:31:06 +0300

    Joagundr,Huzvok

--- src/pdfRoutes.js
+++ src/pdfRoutes.js
@@ -1,35 +1,41 @@
 const pdfRoutes = [
-    {
-      path: '/Meyadi',
-      pdfPath: '/Flip-Book/docs/Meyadi.pdf',
-      audioPath: '/Flip-Book/audio/Meyadi.mp3',
-      transcriptPath: '/Flip-Book/transcript/csvjsonnew.json',
-    },
-    {
-      path: '/Brevinn',
-      pdfPath: '/Flip-Book/docs/Brevinn.pdf',
-      audioPath: '/Flip-Book/audio/Brevinn.mp3',
-      transcriptPath: '/Flip-Book/transcript/Brevinn.json',
-    },
-    {
-      path: '/Traire',
-      pdfPath: '/Flip-Book/docs/Traire.pdf',
-      audioPath: '/Flip-Book/audio/Traire.mp3',
-      transcriptPath: '/Flip-Book/transcript/Traire.json',
-    },
-    {
-      path: '/Isilians',
-      pdfPath: '/Flip-Book/docs/Isilians.pdf',
-      audioPath: '/Flip-Book/audio/Isilians.mp3',
-      transcriptPath: '/Flip-Book/transcript/Isilians.json',
-    },
-    {
-      path: '/Glaze',
-      pdfPath: '/Flip-Book/docs/Glaze.pdf',
-      audioPath: '/Flip-Book/audio/Glaze.mp3',
-      transcriptPath: '/Flip-Book/transcript/Glaze.json',
-    },
-    // Add more routes as needed
-  ];
-  
-  export default pdfRoutes;
+  {
+    path: "/Meyadi",
+    pdfPath: "/Flip-Book/docs/Meyadi.pdf",
+    audioPath: "/Flip-Book/audio/Meyadi.mp3",
+    transcriptPath: "/Flip-Book/transcript/csvjsonnew.json",
+  },
+  {
+    path: "/Brevinn",
+    pdfPath: "/Flip-Book/docs/Brevinn.pdf",
+    audioPath: "/Flip-Book/audio/Brevinn.mp3",
+    transcriptPath: "/Flip-Book/transcript/Brevinn.json",
+  },
+  {
+    path: "/Traire",
+    pdfPath: "/Flip-Book/docs/Traire.pdf",
+    audioPath: "/Flip-Book/audio/Traire.mp3",
+    transcriptPath: "/Flip-Book/transcript/Traire.json",
+  },
+  {
+    path: "/Isilians",
+    pdfPath: "/Flip-Book/docs/Isilians.pdf",
+    audioPath: "/Flip-Book/audio/Isilians.mp3",
+    transcriptPath: "/Flip-Book/transcript/Isilians.json",
+  },
+  {
+    path: "/Glaze",
+    pdfPath: "/Flip-Book/docs/Glaze.pdf",
+    audioPath: "/Flip-Book/audio/Glaze.mp3",
+    transcriptPath: "/Flip-Book/transcript/Glaze.json",
+  },
+  {
+    path: "/Joagundr",
+    pdfPath: "/Flip-Book/docs/Joagundr.pdf",
+    audioPath: "/Flip-Book/audio/Joagundr.mp3",
+    transcriptPath: "/Flip-Book/transcript/Joagundr.json",
+  },
+  // Add more routes as needed
+];
+
+export default pdfRoutes;
